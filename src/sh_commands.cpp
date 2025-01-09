6325b2d59ea9761ab80024932a834b8ddd622308
Joshua <joshuageissler.pharworlds@gmail.com>
2025-01-09 17:08:34 +0100

    updated executables

--- src/sh_commands.cpp
+++ src/sh_commands.cpp
@@ -69,9 +69,12 @@ COMMAND_RESULT cmd_try(std::vector<std::string> args)
     std::string current_path = paths;
     if (std::filesystem::exists(current_path + "/" + args[0]))
     {
-      std::string ex_path = current_path + "/" + args[0];
-      std::cout << ex_path << ": found\n";
-      std::cout << "should be: " << ex_path << '\n';
+      std::string ex_path = current_path + "/";
+      for (auto &arg : args)
+      {
+        ex_path += arg;
+        ex_path += " ";
+      }
       system(ex_path.c_str());
       return SUCCESS;
     }
