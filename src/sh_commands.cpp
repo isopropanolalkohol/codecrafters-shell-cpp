4c6068f5568c6ee80ad3840b473f0cd546c3cd79
Joshua <joshuageissler.pharworlds@gmail.com>
2025-01-21 19:27:11 +0100

    added double quotes

--- src/sh_commands.cpp
+++ src/sh_commands.cpp
@@ -73,7 +73,7 @@ COMMAND_RESULT cmd_try(std::vector<std::string> args)
       std::string ex_path = "";//current_path + "/";
       for (auto arg : args)
       {
-        ex_path += '\'' + arg + "\' ";
+        ex_path += '\"' + arg + "\" ";
       }
       //ex_path = "\"" + ex_path + "\"";
       //std::cout << "the command to be executed: " << ex_path << "\n";
